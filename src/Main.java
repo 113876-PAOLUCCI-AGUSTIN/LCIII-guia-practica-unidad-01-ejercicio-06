fa83bd673e82a82666c89ef6bc6beeb6ea48b331
113876-PAOLUCCI-AGUSTIN <113876@tecnicatura.frc.utn.edu.ar>
2024-03-21 16:55:50 -0300

    IF creado. Resuelto.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,26 @@
+/*
+* 6. Ingresar un año e indicar si ese año es bisiesto.
+* Un año es bisiesto si es múltiplo de 4 y no es múltiplo de 100 o si es
+* múltiplo de 400.
+* MEJOR EXPLICACIÓN: los años bisiestos son los que son divisibles por 4 pero no por 100,
+* y los que siendo divisibles por 100, son divisibles por 400.
+* */
+
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Integer anioIngresado;
+
+        Scanner input = new Scanner(System.in);
+        anioIngresado = input.nextInt();
+        if ((anioIngresado % 4 == 0 && anioIngresado % 100 != 0) ||
+           (anioIngresado % 100 == 0 && anioIngresado % 400 == 0)) // V o V --> true
+        {
+            System.out.println(anioIngresado + ": ES bisiesto.");
+        }
+        else {
+            System.out.println(anioIngresado+ ": NO es bisiesto.");
+        }
     }
 }
